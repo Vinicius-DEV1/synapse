6a70b2887c68a949df6220cfb7d0f23090c82392
VINICIUS-DEV1 <vinicius.cl.dev@gmail.com>
2026-06-28 16:02:25 -0300

    feat(web): hide TabBar when running in browser

--- src/App.tsx
+++ src/App.tsx
@@ -185,8 +185,8 @@ function AppContent() {
 
       {/* Main Area */}
       <div className="flex-1 flex flex-col min-w-0">
-        {/* Tab Bar */}
-        <TabBar />
+        {/* Tab Bar - Only show in Electron */}
+        {navigator.userAgent.toLowerCase().includes('electron') && <TabBar />}
 
         {/* Main Area */}
         <div className="flex-1 overflow-hidden">
