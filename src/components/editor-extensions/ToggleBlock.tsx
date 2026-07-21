f79c8ea0eacfcef6964a187e81c2306dd4909a2e
VINICIUS-DEV1 <vinicius.cl.dev@gmail.com>
2026-07-21 12:46:06 -0300

    feat: handle ArrowUp to jump back to title input in toggle blocks

--- src/components/editor-extensions/ToggleBlock.tsx
+++ src/components/editor-extensions/ToggleBlock.tsx
@@ -84,4 +84,45 @@ export const ToggleBlock = Node.create({
   addNodeView() {
     return ReactNodeViewRenderer(ToggleBlockComponent);
   },
+
+  addKeyboardShortcuts() {
+    return {
+      ArrowUp: ({ editor }) => {
+        const { state } = editor;
+        const { selection } = state;
+        const { $head, empty } = selection;
+
+        if (!empty) return false;
+
+        for (let depth = $head.depth; depth > 0; depth--) {
+          const node = $head.node(depth);
+          if (node.type.name === this.name) {
+            let isAtStart = true;
+            for (let d = $head.depth; d > depth; d--) {
+              if ($head.index(d - 1) !== 0) {
+                isAtStart = false;
+                break;
+              }
+            }
+            if (isAtStart && $head.parentOffset === 0) {
+              const togglePos = $head.before(depth);
+              const domNode = editor.view.nodeDOM(togglePos) as HTMLElement;
+              if (domNode) {
+                const input = domNode.querySelector('input');
+                if (input) {
+                  input.focus();
+                  setTimeout(() => {
+                    input.setSelectionRange(input.value.length, input.value.length);
+                  }, 0);
+                  return true;
+                }
+              }
+            }
+            break;
+          }
+        }
+        return false;
+      }
+    };
+  }
 });
