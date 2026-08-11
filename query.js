93af7ccd64d0f9325baec57959d75fc1a13d3eb4
vinicius-DEV1 <vinicius.cl.dev@gmail.com>
2026-08-11 03:15:22 -0300

    chore: remove unused legacy scripts and outdated manuals

--- query.js
+++
@@ -1,3 +0,0 @@
-const { DatabaseSync } = require('node:sqlite');
-const db = new DatabaseSync('C:/Users/vinic/Downloads/meus apps/caderno/src-tauri/target/release/data/caderno.sqlite');
-console.log(db.prepare("SELECT sql FROM sqlite_master WHERE name = 'videos'").get());
